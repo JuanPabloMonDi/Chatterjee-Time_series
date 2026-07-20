0eb090ef7678d45e282883a548bba1584a6bdc9f
Juan Pablo Montaño Diaz <jmontanod@unal.edu.co>
2026-07-20 16:52:48 -0300

    Update plot code
    Reorganized folder and codes

--- Functions.R
+++ Functions.R
@@ -1,5 +1,23 @@
 #Lets import the necessary packages
 
+
+# List of packages to check
+required_packages <- c(
+  "ppcor", "mgcv", "gplm", "FOCI", "dplyr", "tidyr", 
+  "ggplot2", "XICOR", "forecast", "minerva", "tsDyn"
+)
+
+# Identify which packages are missing
+missing_packages <- required_packages[!(required_packages %in% installed.packages()[, "Package"])]
+
+# Install missing packages if there are any
+if (length(missing_packages) > 0) {
+  install.packages(missing_packages)
+  message("Installed packages: ", paste(missing_packages, collapse = ", "))
+} else {
+  message("All packages are already installed!")
+}
+
 library(ppcor)
 library(mgcv)
 library(gplm)
